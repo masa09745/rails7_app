468fe3b6c120e19e8e25b5a27a3d0f008e520e05
Masa H <masamasa0669@gmail.com>
2022-03-13 13:54:20 +0900

    create ship#index & create user

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  get 'shop/index'
   devise_for :users
+
+  root to: "shop#index"
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
